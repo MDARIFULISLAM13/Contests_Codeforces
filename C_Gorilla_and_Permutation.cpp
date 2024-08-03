b84e0e357146d07384e1fa4a724837ec5f0dad0d
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-08-04 01:26:05 +0600

    solve

---
+++ C_Gorilla_and_Permutation.cpp
@@ -0,0 +1,46 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 11-07-2024
+ * time : 20:53:56
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+void arif()
+{
+    ll n, m, k;
+    cin >> n >> m >> k;
+    for (int i = n; i > m; i--)
+    {
+        cout << i << " ";
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        arif();
+    }
+
+    return 0;
+}
